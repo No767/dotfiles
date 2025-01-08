d94488e4ab67410b4bc3ee784b05ad5a29655e63
No767 <73260931+No767@users.noreply.github.com>
2025-01-08 13:06:20 -0800

    update treesitter ensure installed list

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -12,4 +12,60 @@ return {
       require "configs.lspconfig"
     end,
   },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+        "bash",
+        "powershell",
+        "csv",
+        "dockerfile",
+        "latex",
+        "markdown",
+        "markdown_inline",
+
+        -- Development and utilities
+        "editorconfig",
+        "gitignore",
+        "gitattributes",
+        "jq",
+        "json",
+        "jsonc",
+        "make",
+        "nix",
+        "hurl",
+        "printf",
+        "requirements",
+        "terraform",
+        "yaml",
+        "xml",
+        "toml",
+        "vim",
+        "vimdoc",
+        
+
+        -- Web
+        "html",
+        "css",
+        "scss",
+
+        -- Languages
+        "javascript",
+        "typescript",
+        "python",
+        "lua",
+        "luadoc",
+        "groovy",
+        "java",
+        "kotlin",
+        "c",
+        "cpp",
+        "cmake",
+        "rust",
+        "sql",
+        "svelte"
+  		},
+  	},
+  }
 }
