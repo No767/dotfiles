8b5a416feb89d55e06f69f0b02150d14e6890ca4
No767 <73260931+No767@users.noreply.github.com>
2025-01-31 02:23:07 -0800

    Add all LSPs and highlighters by default

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -2,12 +2,53 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
-
--- EXAMPLE
-local servers = { "html", "cssls", "ts_ls", "clangd" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
+local servers = {
+  "actionlint",
+  "bash-language-server",
+  "biome",
+  "clang-format",
+  "clangd",
+  "cpptools",
+  "cspell",
+  "css-lsp",
+  "cssmodules-language-server",
+  "debugpy",
+  "docker-compose-language-server",
+  "editorconfig-checker",
+  "gitleaks",
+  "gradle-language-server",
+  "groovy-language-server",
+  "html-lsp",
+  "htmlhint",
+  "jq",
+  "js-debug-adapter",
+  "json-lsp",
+  "kotlin-language-server",
+  "lua-language-server",
+  "markdownlint-cli2",
+  "marksman",
+  "mdx-analyzer",
+  "nixpkgs-fmt",
+  "nxls",
+  "pyright",
+  "rust-analyzer",
+  "sonarlint-language-server",
+  "sqlfluff",
+  "sqlls",
+  "stylelint",
+  "stylelint-lsp",
+  "stylua",
+  "tailwindcss-language-server",
+  "terraform-ls",
+  "tree-sitter-cli",
+  "typescript-language-server",
+  "yaml-language-server",
+  "yamlfmt",
+  "yamllint",
+}
+
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = nvlsp.on_attach,
